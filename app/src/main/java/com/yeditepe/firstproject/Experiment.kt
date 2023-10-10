6a805aabcc232314c81a99df1e1f4ee8eb607112
tbf216 <engin.kandiran@yeditepe.edu.tr>
2023-10-10 11:21:49 +0300

    This is a commit from other team player

--- app/src/main/java/com/yeditepe/firstproject/Experiment.kt
+++ app/src/main/java/com/yeditepe/firstproject/Experiment.kt
@@ -5,4 +5,5 @@ fun main(){
     println("Hello Github")
     println("This is second addition")
     println("This is  a new experimental branch")
+    println("This is comment done by other people")
 }
